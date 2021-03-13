6dd736a6b0be85e22f1a78cfe9944e34cbb98d10
lnnolin <lnnolin@massart.edu>
2021-03-12 23:01:46 -0500

    fixed scripting in scenes 3b and 5a

--- cdan221_storyA/Assets/Scripts/DialogueScene3b.cs
+++ cdan221_storyA/Assets/Scripts/DialogueScene3b.cs
@@ -20,7 +20,7 @@ public class DialogueScene1 : MonoBehaviour {
         public GameObject Choice1a;
         public GameObject Choice1b;
         public GameObject NextScene1Button;
-        public GameObject NextScene2Button;
+        //public GameObject NextScene2Button;
         public GameObject nextButton;
        //public GameObject gameHandler;
        //public AudioSource audioSource;
@@ -33,7 +33,7 @@ void Start(){         // initial visibility settings
         Choice1a.SetActive(false);
         Choice1b.SetActive(false);
         NextScene1Button.SetActive(false);
-        NextScene2Button.SetActive(false);
+        //NextScene2Button.SetActive(false);
         nextButton.SetActive(true);
    }
 
@@ -56,7 +56,7 @@ public void talking(){         // main story function. Players hit next to progr
                 dialogue.SetActive(true);
                 Char1name.text = "GUARD 1";
                 Char1speech.text = "I still can’t believe how heavy this barrel is! I mean, couldn’t we have at least seen what was in it before hauling it to the docks?";
-                Char2name.text = "GUARD 2";
+                Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt ==3){
@@ -64,13 +64,13 @@ public void talking(){         // main story function. Players hit next to progr
 			    ArtChar2.SetActive(false);
                 Char1name.text = "GUARD 1";
                 Char1speech.text = "I still can’t believe how heavy this barrel is! I mean, couldn’t we have at least seen what was in it before hauling it to the docks? \n What if it’s just full of something useless like bricks! And that rancid smell! It’s like there’s a family of dead skunks in here!";
-                Char2name.text = "GUARD 2";
+                Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt == 4){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
-                Char1name.text = "GUARD 1";
+                Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "GUARD 2";
                 Char2speech.text = "I dunno Rowan, I wouldn’t question the king and queen.";
@@ -78,7 +78,7 @@ public void talking(){         // main story function. Players hit next to progr
        else if (primeInt == 5){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
-                Char1name.text = "GUARD 1";
+                Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "GUARD 2";
                 Char2speech.text = "I dunno Rowan, I wouldn’t question the king and queen. \n They were very clear, “Get rid of any and all barrels! Our boy used to love counting them, and now he’s gone!” Blah blah blah!";
@@ -88,7 +88,7 @@ public void talking(){         // main story function. Players hit next to progr
 			    ArtChar2.SetActive(false);
 				Char1name.text = "ROWAN";
                 Char1speech.text = "Well Peter, no disrespect to the late prince, but I mean, he liked barrels so now we have to get rid of all them? Where’s the logic in that?";
-                Char2name.text = "PETER";
+                Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt ==7){
@@ -96,21 +96,21 @@ public void talking(){         // main story function. Players hit next to progr
 			    ArtChar2.SetActive(false);
 				Char1name.text = "ROWAN";
                 Char1speech.text = "Well Peter, no disrespect to the late prince, but I mean, he liked barrels so now we have to get rid of all them? Where’s the logic in that? \n What kind of kid did the king raise if his favorite thing in life was counting bloody barrels?";
-                Char2name.text = "PETER";
+                Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt == 8){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
-				Char1name.text = "ROWAN";
-                Char1speech.text =
-                Char2name.text = "ROWAN";
+				Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "PETER";
                 Char2speech.text = "I feel you. It’s definitely an odd request. But I’m not trying to get on their bad side right now.";
         }
 		 else if (primeInt == 9){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
-				Char1name.text = "ROWAN";
+				Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "PETER";
                 Char2speech.text = "I feel you. It’s definitely an odd request. But I’m not trying to get on their bad side right now. \n It’s been 2 weeks now, and still, no trace of the poor prince’s murderer. I’m sure the king will jump at any opportunity to bring anyone who gives him any lip straight to the gallows.";
@@ -120,7 +120,7 @@ public void talking(){         // main story function. Players hit next to progr
 			    ArtChar2.SetActive(false);
 				Char1name.text = "ROWAN";
                 Char1speech.text = "Bah! This is rediculous. I'm not lugging around this barrel any longer!";
-                Char2name.text = "PETER";
+                Char2name.text = "";
                 Char2speech.text = "";
         }
 		else if (primeInt == 11){
@@ -128,13 +128,13 @@ public void talking(){         // main story function. Players hit next to progr
 			    ArtChar2.SetActive(false);
 				Char1name.text = "ROWAN";
                 Char1speech.text = "Bah! This is rediculous. I'm not lugging around this barrel any longer! \n I mean, what the hell was Agatha cooking with? Ogre tusks?! I’d rather take my luck hunting down that dirty murderer than move this thing another inch!";
-                Char2name.text = "PETER";
+                Char2name.text = "";
                 Char2speech.text = "";
         }
 		else if (primeInt == 12){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
-				Char1name.text = "ROWAN";
+				Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "PETER";
                 Char2speech.text = "You're really gonna throw a tantrum right now?";
@@ -150,42 +150,107 @@ public void talking(){         // main story function. Players hit next to progr
 			    ArtChar2.SetActive(false);
 				Char1name.text = "ROWAN";
                 Char1speech.text = "Damn straight! This god damn bunion is killing me! To hell with this stupid barrel! What is even inside this thing?!";
-                Char2name.text = "PETER";
+                Char2name.text = "";
                 Char2speech.text = "";     
         }
-       else if (primeInt == 101){
+		
+		else if (primeInt == 101){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
+				Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "PETER";
+                Char2speech.text = "Rowan, don't!";     
+		}
+        
+		else if (primeInt == 102){
+                ArtChar1.SetActive(true);
+			    ArtChar2.SetActive(false);
 				Char1name.text = "ROWAN";
+                Char1speech.text = "My god...";
+                Char2name.text = "";
+                Char2speech.text = "";   
+		}				
+        
+		else if (primeInt == 103){
+                ArtChar1.SetActive(false);
+			    ArtChar2.SetActive(true);
+				Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "PETER";
-                Char2speech.text = "Rowan, don't!";
+                Char2speech.text = "What? What is it?";     
+		}
+        
+		else if (primeInt == 104){
+                ArtChar1.SetActive(true);
+			    ArtChar2.SetActive(false);
+				Char1name.text = "ROWAN";
+                Char1speech.text = "Take a look for yourself.";
+                Char2name.text = "";
+                Char2speech.text = "";  
+				// Turn off "Next" button, turn on "SceneChange" button
                 nextButton.SetActive(false);
                 allowSpace = false;
-                NextScene1Button.SetActive(true);
-        }
-
+                NextScene1Button.SetActive(true); // function Choice1aFunct()
+               
+		}				
+        
+		// ENCOUNTER AFTER CHOICE #2
        else if (primeInt == 200){
-                Char1name.text = "ROWAN";
-                Char1speech.text = "";
-                Char2name.text = "PETER";
-                Char2speech.text = "";               
+                ArtChar1.SetActive(true);
+			    ArtChar2.SetActive(false);
+				Char1name.text = "ROWAN";
+                Char1speech.text = "Ugh, whatever. Let's just pick up the pace at least.";
+                Char2name.text = "";
+                Char2speech.text = "";     
         }
-       else if (primeInt == 201){
-                Char1name.text = "ROWAN";
+		
+		else if (primeInt == 201){
+                ArtChar1.SetActive(false);
+			    ArtChar2.SetActive(true);
+				Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "PETER";
-                Char2speech.text = "";
+                Char2speech.text = "Slow it down! I'm gonna trip if we keep up this speed!";     
+		}
+        
+		else if (primeInt == 202){
+                ArtChar1.SetActive(true);
+			    ArtChar2.SetActive(false);
+				Char1name.text = "ROWAN";
+                Char1speech.text = "You want some cheese with that wi-WOAH!";
+                Char2name.text = "";
+                Char2speech.text = "";   
+		}				
+        
+		else if (primeInt == 203){
+                ArtChar1.SetActive(false);
+			    ArtChar2.SetActive(true);
+				Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "PETER";
+                Char2speech.text = "You moron, you couldn’t even handle your own speed! Now look, the barrel is all smashed, and… and. Oh my.";     
+		}
+		
+		else if (primeInt == 204){
+                ArtChar1.SetActive(true);
+			    ArtChar2.SetActive(false);
+				Char1name.text = "ROWAN";
+                Char1speech.text = "Is this what I think it is?";
+                Char2name.text = "";
+                Char2speech.text = ""; 
+// Turn off "Next" button, turn on "SceneChange" button
                 nextButton.SetActive(false);
                 allowSpace = false;
-                NextScene2Button.SetActive(true);
-        }
-     }
+                NextScene1Button.SetActive(true); // function Choice1aFunct()				
+		}		
+		
+}
 // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
         public void Choice1aFunct(){
                 Char1name.text = "ROWAN";
-                Char1speech.text = "";
-                Char2name.text = "PETER";
+                Char1speech.text = "Damn straight!";
+                Char2name.text = "";
                 Char2speech.text = "";
                 primeInt = 99;
                 Choice1a.SetActive(false);
@@ -195,8 +260,8 @@ public void talking(){         // main story function. Players hit next to progr
         }
         public void Choice1bFunct(){
                 Char1name.text = "ROWAN";
-                Char1speech.text = "";
-                Char2name.text = "PETER";
+                Char1speech.text = "Ugh, whatever.";
+                Char2name.text = "";
                 Char2speech.text = "";
                 primeInt = 199;
                 Choice1a.SetActive(false);
@@ -206,7 +271,7 @@ public void talking(){         // main story function. Players hit next to progr
         }
 
         public void SceneChange2a(){
-               SceneManager.LoadScene("Scene2a");
+               SceneManager.LoadScene("SceneEnd");
         }
         public void SceneChange2b(){
                 SceneManager.LoadScene("Scene2b");
--- cdan221_storyA/Assets/Scripts/DialogueScene5a.cs
+++ cdan221_storyA/Assets/Scripts/DialogueScene5a.cs
@@ -64,13 +64,13 @@ public void talking(){         // main story function. Players hit next to progr
                 dialogue.SetActive(true);
                 Char1name.text = "KEEPER";
                 Char1speech.text = "So, you're gonna take on Crusher, huh? Aren't you confident? Oh well, he's all yours!";
-                Char2name.text = "CRUSHER";
+                Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt ==3){
 				ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
-                Char1name.text = "KEEPER";
+                Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "CRUSHER";
                 Char2speech.text = "Bring it on! Bring it on then, tough guy! I'll turn you into dust! This is my fiftieth fight of the day, and I've won every single one of 'em so far! I can scrap with the best! Let's go!";
@@ -79,9 +79,9 @@ public void talking(){         // main story function. Players hit next to progr
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(false);
 				ArtChar3.SetActive(true);
-				Char1name.text = "KEEPER";
+				Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "CRUSHER";
+                Char2name.text = "";
                 Char2speech.text = "";
                 Char3name.text = "YOU";
                 Char3speech.text = "This guy must be pretty tough. He won fifty fights in a row? How?";
@@ -90,20 +90,20 @@ public void talking(){         // main story function. Players hit next to progr
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
 				ArtChar3.SetActive(false);
-				Char1name.text = "KEEPER";
+				Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "CRUSHER";
                 Char2speech.text = "Eat my fists of fury!!! HYAHHHH!!!!!";
-                Char3name.text = "YOU";
+                Char3name.text = "";
                 Char3speech.text = "";
         }
        else if (primeInt == 6){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(false);
 				ArtChar3.SetActive(true);
-				Char1name.text = "KEEPER";
+				Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "CRUSHER";
+                Char2name.text = "";
                 Char2speech.text = "";
                 Char3name.text = "YOU";
                 Char3speech.text = "EEK!";
@@ -112,9 +112,9 @@ public void talking(){         // main story function. Players hit next to progr
 				ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
 				ArtChar3.SetActive(false);
-				Char1name.text = "KEEPER";
+				Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "CRUSHER";
+                Char2name.text = "";
                 Char2speech.text = "";
                 Char3name.text = "YOU";
                 Char3speech.text = "You lightly slap Crusher as he flies towards you, instantly disintegrating him into dust. A bell rings out in the room.";
@@ -125,20 +125,20 @@ public void talking(){         // main story function. Players hit next to progr
 				ArtChar3.SetActive(false);
 				Char1name.text = "KEEPER";
                 Char1speech.text = "Wow, that took longer than it usually does. Hey, can somebody get the dustpan and come clean Crusher up?";
-                Char2name.text = "DUST PILE";
+                Char2name.text = "";
                 Char2speech.text = "";
-                Char3name.text = "YOU";
+                Char3name.text = "";
                 Char3speech.text = "";
         }
 		 else if (primeInt == 9){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(true);
 				ArtChar3.SetActive(false);
-				Char1name.text = "KEEPER";
+				Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "DUST PILE";
                 Char2speech.text = "Hey! No fair! I wasn't ready! Rematch! Rematch!";
-                Char3name.text = "YOU";
+                Char3name.text = "";
                 Char3speech.text = "";
 		}
 		else if (primeInt == 10){
@@ -147,18 +147,18 @@ public void talking(){         // main story function. Players hit next to progr
 				ArtChar3.SetActive(false);
 				Char1name.text = "KEEPER";
                 Char1speech.text = "He'll be okay, we can just glue him back together. We have to do this after all his fights.";
-                Char2name.text = "DUST PILE";
+                Char2name.text = "";
                 Char2speech.text = "";
-                Char3name.text = "YOU";
+                Char3name.text = "";
                 Char3speech.text = "";
         }
 		else if (primeInt == 11){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(false);
 				ArtChar3.SetActive(true);
-				Char1name.text = "KEEPER";
+				Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "DUST PILE";
+                Char2name.text = "";
                 Char2speech.text = "";
                 Char3name.text = "YOU";
                 Char3speech.text = "Where's my prize?";
@@ -169,9 +169,9 @@ public void talking(){         // main story function. Players hit next to progr
 				ArtChar3.SetActive(false);
 				Char1name.text = "KEEPER";
                 Char1speech.text = "Not one for conversation, huh? Fine, fine. Here you go.";
-                Char2name.text = "DUST PILE";
+                Char2name.text = "";
                 Char2speech.text = "";
-                Char3name.text = "YOU";
+                Char3name.text = "";
                 Char3speech.text = "";
         }
 		else if (primeInt == 13){
@@ -180,9 +180,9 @@ public void talking(){         // main story function. Players hit next to progr
 				ArtChar3.SetActive(false);
 				Char1name.text = "KEEPER";
                 Char1speech.text = "Not one for conversation, huh? Fine, fine. Here you go. \n The dungeon keeper hands over the golden chalice, and your time machine back to the future. Right as you grab the cup, you hear the royal guards enter the dungeon.";
-                Char2name.text = "DUST PILE";
+                Char2name.text = "";
                 Char2speech.text = "";
-                Char3name.text = "YOU";
+                Char3name.text = "";
                 Char3speech.text = "";
         }
 		else if (primeInt == 14){
@@ -190,11 +190,11 @@ public void talking(){         // main story function. Players hit next to progr
 			    ArtChar2.SetActive(false);
 				ArtChar3.SetActive(false);
 				ArtChar4.SetActive(true);
-				Char1name.text = "KEEPER";
+				Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "DUST PILE";
+                Char2name.text = "";
                 Char2speech.text = "";
-                Char3name.text = "YOU";
+                Char3name.text = "";
                 Char3speech.text = "";
 				Char4name.text = "GUARD";
                 Char4speech.text = "The prince has been killed! Place the castle on lockdown immediately!";
@@ -204,102 +204,30 @@ public void talking(){         // main story function. Players hit next to progr
 			    ArtChar2.SetActive(false);
 				ArtChar3.SetActive(true);
 				ArtChar4.SetActive(false);
-				Char1name.text = "KEEPER";
+				Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "DUST PILE";
+                Char2name.text = "";
                 Char2speech.text = "";
                 Char3name.text = "YOU";
                 Char3speech.text = "Alright, time to go!";
-				Char4name.text = "GUARD";
+				Char4name.text = "";
                 Char4speech.text = "";
         }else if (primeInt == 16){
                 ArtChar1.SetActive(false);
 			    ArtChar2.SetActive(false);
 				ArtChar3.SetActive(true);
 				ArtChar4.SetActive(false);
-				Char1name.text = "KEEPER";
+				Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "DUST PILE";
+                Char2name.text = "";
                 Char2speech.text = "";
                 Char3name.text = "YOU";
                 Char3speech.text = "Alright, time to go! \n You press the gems adorning the chalice in a specific sequence and white light glows from the chalice. It grows brighter as it fills the room, blinding the guards. It grows brighter and brighter, until...";
-				Char4name.text = "GUARD";
+				Char4name.text = "";
                 Char4speech.text = "";
         }
-		else if (primeInt == 15){
-                ArtChar1.SetActive(false);
-			    ArtChar2.SetActive(true);
-				Char1name.text = "ROWAN";
-                Char1speech.text = "";
-                Char2name.text = "PETER";
-                Char2speech.text = "You're really gonna throw a tantrum right now?";
-                // Turn off "Next" button, turn on "Choice" buttons
-                nextButton.SetActive(false);
-                allowSpace = false;
-                Choice1a.SetActive(true); // function Choice1aFunct()
-                Choice1b.SetActive(true); // function Choice1bFunct()
-        }
-// ENCOUNTER AFTER CHOICE #1
-       else if (primeInt == 100){
-                ArtChar1.SetActive(true);
-			    ArtChar2.SetActive(false);
-				Char1name.text = "ROWAN";
-                Char1speech.text = "Damn straight! This god damn bunion is killing me! To hell with this stupid barrel! What is even inside this thing?!";
-                Char2name.text = "PETER";
-                Char2speech.text = "";     
-        }
-       else if (primeInt == 101){
-                ArtChar1.SetActive(false);
-			    ArtChar2.SetActive(true);
-				Char1name.text = "ROWAN";
-                Char1speech.text = "";
-                Char2name.text = "PETER";
-                Char2speech.text = "Rowan, don't!";
-                nextButton.SetActive(false);
-                allowSpace = false;
-                NextScene1Button.SetActive(true);
-        }
-
-       else if (primeInt == 200){
-                Char1name.text = "ROWAN";
-                Char1speech.text = "";
-                Char2name.text = "PETER";
-                Char2speech.text = "";               
-        }
-       else if (primeInt == 201){
-                Char1name.text = "ROWAN";
-                Char1speech.text = "";
-                Char2name.text = "PETER";
-                Char2speech.text = "";
-                nextButton.SetActive(false);
-                allowSpace = false;
-                NextScene2Button.SetActive(true);
-        }
-     }
-// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
-        public void Choice1aFunct(){
-                Char1name.text = "ROWAN";
-                Char1speech.text = "";
-                Char2name.text = "PETER";
-                Char2speech.text = "";
-                primeInt = 99;
-                Choice1a.SetActive(false);
-                Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
-        }
-        public void Choice1bFunct(){
-                Char1name.text = "ROWAN";
-                Char1speech.text = "";
-                Char2name.text = "PETER";
-                Char2speech.text = "";
-                primeInt = 199;
-                Choice1a.SetActive(false);
-                Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
-        }
-
+		
+}
         public void SceneChange2a(){
                SceneManager.LoadScene("Scene2a");
         }
